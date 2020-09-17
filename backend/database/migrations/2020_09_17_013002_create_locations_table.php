5f362190d82c55bcef866f49691afda33605f5ea
William Trindade <williamtrindade777@gmail.com>
2020-09-16 23:21:25 -0300

    fix(): Adjusting migrations

--- backend/database/migrations/2020_09_17_013002_create_locations_table.php
+++ backend/database/migrations/2020_09_17_013002_create_locations_table.php
@@ -16,6 +16,7 @@ class CreateLocationsTable extends Migration
     public function up()
     {
         Schema::create('locations', function (Blueprint $table) {
+            $table->id();
             $table->string('lat');
             $table->string('lng');
 
