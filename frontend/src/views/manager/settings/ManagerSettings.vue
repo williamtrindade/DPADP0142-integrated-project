759e26a5688376fdb3a93bff1d5ff559a5d4789a
William Trindade <williamtrindade777@gmail.com>
2020-10-11 15:23:28 -0300

    Global loader spinner

---
+++ frontend/src/views/manager/settings/ManagerSettings.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="manager-settings">
+        <sidebar></sidebar>
+        <topbar></topbar>
+        <div class="content">
+            <h1 class="title-black">Configurações</h1>
+            <UserSettings></UserSettings>
+            <AccountSettings></AccountSettings>
+        </div>
+    </div>
+</template>
+
+<script>
+import ManagerSidebar from '@/components/manager/ManagerSidebar'
+import ManagerTopbar from '@/components/manager/ManagerTopbar'
+import UserSettings from '@/components/settings/UserSettings'
+import AccountSettings from '@/components/settings/AccountSettings'
+
+export default {
+    name: 'ManagerSettings',
+    components: {
+        sidebar: ManagerSidebar,
+        topbar: ManagerTopbar,
+        UserSettings,
+        AccountSettings
+    }
+}
+</script>
+
+<style scoped>
+</style>
